e7380a02e0b2fa739dfa3d6541059e9183fa1227
ngchongin <ngchongin@gmail.com>
2023-05-30 23:59:02 -0400

    complete features

--- Assignment/Pack.cs
+++ Assignment/Pack.cs
@@ -2,12 +2,17 @@
 
 public class Pack
 {
-    private readonly InventoryItem[] _items; // You can use another data structure here if you prefer.
+    const float EPSILON = 0.0001f;
+
+    private readonly List<InventoryItem> _items; // You can use another data structure here if you prefer.
     // You may need another private member variable if you use an array data structure.
 
     private readonly int _maxCount;
     private readonly float _maxVolume;
     private readonly float _maxWeight;
+
+    private float _currentVolume;
+    private float _currentWeight;
 
     // Default constructor sets the maxCount to 10 
     // maxVolume to 20 
@@ -17,9 +22,16 @@ public class Pack
     // This constructor is not complete, but it is a good start.
     public Pack(int maxCount, float maxVolume, float maxWeight)
     {
+        if (maxCount < 0 || maxVolume < EPSILON || maxWeight < EPSILON)
+        {
+            throw new ArgumentOutOfRangeException($"An item can't have {maxCount} max count,  {maxVolume} maxvolume or {maxWeight} max weight");
+        }
         _maxCount = maxCount;
         _maxVolume = maxVolume;
         _maxWeight = maxWeight;
+        _items = new List<InventoryItem>();
+        _currentVolume = 0f;
+        _currentWeight = 0f;
     }
 
     // This is called a getter
@@ -28,31 +40,62 @@ public class Pack
         return _maxCount;
     }
 
+    //add item to the list
     public bool Add(InventoryItem item)
     {
+        //check the count will over or not
+        if (GetCurrentItemsCount() >= _maxCount)
+        {
+            Console.WriteLine("Cannot more items now because the array already put the maximum count of items into it.");
+            return false;
+        }
+
         float weight = item.GetWeight();
-        throw new NotImplementedException();
+        float volume = item.GetVolume();
+        _currentWeight += weight;
+        _currentVolume += volume;
+
+        // check the weight or volume will over or not.
+        if (_currentWeight > _maxWeight || _currentVolume > _maxVolume)
+        {
+            Console.WriteLine($"Cannot add this item:{item.GetName} because it will over max weight or max volume.");
+            return false;
+        }
+        _items.Add(item);
+        return true;
     }
 
     // Implement this class
     public override string ToString()
     {
-        throw new NotImplementedException();
+        return ($"Pack is currently at {GetCurrentItemsCount()}/{_maxCount} items, {_currentWeight}/{_maxWeight} weight, and {_currentVolume}/{_maxVolume} volume");
+    }
+
+    //return the amount the items
+    private int GetCurrentItemsCount()
+    {
+        return _items.Count;
     }
 }
 
 // Come back to this once we learn about abstract classes.
 public class InventoryItem
 {
+    private readonly string _name;
     private readonly float _volume;
     private readonly float _weight;
 
-    public InventoryItem(float volume, float weight)
+    public InventoryItem(string name,  float volume, float weight)
     {
         if (volume <= 0f || weight <= 0f)
         {
             throw new ArgumentOutOfRangeException($"An item can't have {volume} volume or {weight} weight");
         }
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new ArgumentOutOfRangeException($"An item can't have empty name.");
+        }
+        _name = name;
         _volume = volume;
         _weight = weight;
     }
@@ -63,25 +106,50 @@ public class InventoryItem
         return _volume;
     }
 
+    // return weight
     public float GetWeight()
     {
         return _weight;
+    }
+
+    // return name
+    public string GetName()
+    {
+        return _name;
     }
 }
 
 // Implement these classes - each inherits from InventoryItem
 // 1 line of code each - call base class constructor with appropriate arguments
-class Arrow : InventoryItem
+public class Arrow : InventoryItem
 {
-    public Arrow() : base(0.05f, 0.1f) { }
+    public Arrow() : base("Arrow", 0.05f, 0.1f) { }
+
+    //add contructor to test 
+    public Arrow(float volume, float weight) : base("Arrow", volume, weight) { }
 }
 
-class Bow : InventoryItem
+public class Bow : InventoryItem
 {
-    public Bow() : base(1f, 4f) { }
+    public Bow() : base("Bow", 1f, 4f) { }
 }
 
-class Rope : InventoryItem { }
-class Water : InventoryItem { }
-class Food : InventoryItem { }
-class Sword : InventoryItem { }
+public class Rope : InventoryItem
+{
+    public Rope() : base("Rope", 1f, 1.5f) { }
+}
+
+public class Water : InventoryItem
+{
+    public Water() : base("Water", 2f, 3f) { }
+}
+
+public class Food : InventoryItem
+{
+    public Food() : base("Food", 1f, 0.5f) { }
+}
+
+public class Sword : InventoryItem
+{
+    public Sword() : base("Sword", 5f, 3f) { }
+}
--- AssignmentTest/AssignmentTest.cs
+++ AssignmentTest/AssignmentTest.cs
@@ -3,7 +3,7 @@
 namespace AssignmentTest
 {
     [TestClass]
-    public class AssignmentTests
+    public class PackTests
     {
         [TestMethod]
         public void ConstructorTest()
@@ -14,6 +14,82 @@ namespace AssignmentTest
             Pack pack = new(PackMaxItems, PackMaxVolume, PackMaxWeight);
 
             Assert.AreEqual(pack.GetMaxCount(), PackMaxItems);
+        }
+
+        [TestMethod]
+        public void ConstructorNegativeMaxCountTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Pack(-10, 20, 20));
+        }
+
+        [TestMethod]
+        public void ConstructorNegativeMaxVoloumeTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Pack(10, -20, 20));
+        }
+
+        [TestMethod]
+        public void ConstructorNegativeMaxWightTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Pack(10, 20, -20));
+        }
+
+        [TestMethod]
+        public void AddSignleItemTestToAnEmptyPack()
+        {
+            Pack pack = new(10, 20, 30);
+            bool result = pack.Add(new Arrow());
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void OverMaxCountTest()
+        {
+            Pack pack = new(1, 20, 30);
+            pack.Add(new Arrow());
+            Assert.AreEqual(false, pack.Add(new Arrow()));
+        }
+
+        [TestMethod]
+        public void OverMaxWeightTest()
+        {
+            Pack pack = new(10, 20, 2);
+            Assert.AreEqual(false, pack.Add(new Sword()));
+        }
+
+        [TestMethod]
+        public void OverMaxVolumeTest()
+        {
+            Pack pack = new(10, 2, 30);
+            Assert.AreEqual(false, pack.Add(new Sword()));
+        }
+
+        [TestMethod]
+        public void AddItemWithNegativeWeight()
+        {
+            Pack pack = new(10, 2, 30);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => pack.Add(new Arrow(10, -20)));
+        }
+
+        [TestMethod]
+        public void AddItemWithNegativeVolume()
+        {
+            Pack pack = new(10, 2, 30);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => pack.Add(new Arrow(-10, 20)));
+        }
+
+        [TestMethod]
+        public void AddItemWithZeroWeight()
+        {
+            Pack pack = new(10, 2, 30);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => pack.Add(new Arrow(10, 0)));
+        }
+
+        [TestMethod]
+        public void AddItemWithZeroVolume()
+        {
+            Pack pack = new(10, 2, 30);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => pack.Add(new Arrow(0, 20)));
         }
     }
 }
